78a85f3c816465006c0d45b2037383e724a90d10
xvin <xvin@bupt.edu.cn>
2024-12-23 17:09:43 +0800

    add deno adpter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel";
+import deno from "@deno/astro-adapter";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react(), tailwind()],
-  adapter: vercel(),
+  adapter: deno(),
 });
