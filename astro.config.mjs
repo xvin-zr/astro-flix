465686791711c53b74b2bdc9ca5f1f80c7b8b817
xvin <xvin@bupt.edu.cn>
2024-12-23 17:02:12 +0800

    move to vercel adpter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import deno from "@deno/astro-adapter";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [react(), tailwind()],
-  adapter: deno(),
+  adapter: vercel(),
 });
