a8e6f420e8d5233a3dece3ab52eeddbf7e25871f
xvin <xvin@bupt.edu.cn>
2025-02-24 16:55:27 +0800

    add path alias for vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,22 @@ import tailwind from "@astrojs/tailwind";
 
 // import deno from "@deno/astro-adapter";
 import vercel from "@astrojs/vercel";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react(), tailwind()],
   adapter: vercel(),
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
 });
